77833fd406f58cfbd5392a68f9791e7ff974c56d
elvirafirmansyah <freddyfirmansyah25@gmail.com>
2023-06-20 23:30:05 +0700

    fix: button border ui

--- src/components/Title.js
+++ src/components/Title.js
@@ -16,9 +16,9 @@ const Title = (props) => {
     >
       <Heading color={useColorModeValue("green.400", "green.300")}>{props.title}</Heading>
       <Text color={useColorModeValue("gray.600", "gray.400")} fontWeight={"600"} fontSize={"lg"} pb="5"> 
-        {props.dp} <a href={props.url1} target={"blank"}><chakra.span color={useColorModeValue("black", "white")}>{props.status1}</chakra.span></a>
+        {props.dp} <a href={props.url1} target={"blank"}><chakra.span color={useColorModeValue("black", "white")} _hover={{ textDecoration: "underline", textDecorationColor: "green.300", textDecorationThickness: "2px", textUnderlineOffset: 5 }}> {props.status1}</chakra.span></a>
         {props.status2 ? " and " : ''}
-        {props.status2 && props.url2 ? <a href={props.url2} target={"blank"}><chakra.span color="black" _dark={{color: "white"}}>{props.status2}</chakra.span></a> : null }
+        {props.status2 && props.url2 ? <a href={props.url2} target={"blank"}><chakra.span color="black" _dark={{ color: "white" }} _hover={{ textDecoration: "underline", textDecorationColor: "green.300", textDecorationThickness: "2px", textUnderlineOffset: 5 }}>{props.status2}</chakra.span></a> : null }
       </Text>
       {props.divider}
     </Stack>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -119,7 +119,7 @@ export default function Home() {
                 target="_blank"
                 rel="noreferrer"
               >
-                <Button bg="green.400" color="white" _hover={{ bg: "green.500"}}>Resume</Button>
+                <Button bg="green.400" color="white" _hover={{ bg: "green.500"}} _focus={{boxShadow: "none"}}>Resume</Button>
               </a>
 
               <Button border="1px" borderColor={useColorModeValue("gray.200", "whiteAlpha.200")} _hover={{ bg: useColorModeValue("#F1F2F6", "whiteAlpha.50")}} bg="transparent" display="flex" alignItems={"center"}  >
